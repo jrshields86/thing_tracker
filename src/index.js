83fef42a5ecb99c16961c1f5959812bab0998f29
jrshields86 <jrshields86@gmail.com>
2023-09-25 00:23:16 -0600

    displaying things correctly

--- src/index.js
+++ src/index.js
@@ -16,18 +16,28 @@ const App = ()=> {
     fetchUsers();
   }, []);
 
+  useEffect(() => {
+    const fetchThings = async()=>{
+      const response = await axios.get('/api/things');
+      setThings(response.data)
+    }
+    fetchThings();
+  }, []);
+
   return (
     <div>
-    <h1>FullStack Template</h1>
+    <h1>Thing Tracker</h1>
     <main>
       <div>
         <h2>Users ({ users.length })</h2>
         <ul>
           {
             users.map( user => {
+              const usersThings = things.filter(thing => thing.user_id === user.id);
               return (
                 <li key={ user.id }>
                   { user.name }
+                  ({ usersThings.length })
                 </li>
               )
             })
@@ -36,6 +46,28 @@ const App = ()=> {
       </div>
       <div>
         <h2>Things ({ things.length })</h2>
+        <ul>
+          {
+            things.map( thing => {
+              return (
+                <li key={ thing.id }>
+                  { thing.name }
+                  <ul>
+                    {
+                      users.map( user => {
+                        return (
+                          <li key={user.id} className={ thing.user_id === user.id ? 'owner': ''}>
+                            { user.name }
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
     </main>
     </div> 
